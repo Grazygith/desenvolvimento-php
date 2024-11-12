40818e8fd2f682ad16ee0fe8e6b3fd906e07ea62
Graziele Alves Soares <graziele04@hotmail.com>
2024-11-12 09:09:06 -0300

    commit inicial

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('BASE', 'cadastro');
+
+$conn = new mysqli(HOST, USER, PASS, BASE);
+
+// A classe mysql representa a conexão entre o PHP e um banco de dados mysql. ela esta incluida nas versões 5 e posteriores do PHP
+
+
+
+?>
